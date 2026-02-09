4b4fd45e173ca7e0ffb9119651771432e126e86e
softgenai <admin@softgen.ai>
2026-02-09 15:53:31 +0000

    Updates

--- src/components/financial/DepositInstallmentsTable.tsx
+++ src/components/financial/DepositInstallmentsTable.tsx
@@ -646,7 +646,7 @@ export function DepositInstallmentsTable({
                       </TableCell>
                     </TableRow>
                   ))
-                ))}
+                )}
               </TableBody>
             </Table>
           </div>
