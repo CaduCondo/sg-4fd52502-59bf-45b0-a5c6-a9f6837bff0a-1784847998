8f2f4dbac60817c465001c8e9d1a6faaa224dca8
softgenai <admin@softgen.ai>
2026-07-09 18:27:57 +0000

    fix(tenant): add new status to type definition

--- src/types/index.ts
+++ src/types/index.ts
@@ -147,7 +147,7 @@ export interface Tenant {
   rg?: string;
   cpf?: string;
   cnpj?: string;
-  status: "active" | "inactive" | "rented" | "late" | "debt";
+  status: "new" | "active" | "inactive" | "rented" | "late" | "debt";
   cep?: string;
   street?: string;
   number?: string;
