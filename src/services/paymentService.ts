e25f9a2798588030384bd9b38c170a0c327b0376
softgenai <admin@softgen.ai>
2026-07-18 00:43:40 +0000

    fix(payment): cast return object to Payment type

--- src/services/paymentService.ts
+++ src/services/paymentService.ts
@@ -202,7 +202,7 @@ export const create = async (payment: Partial<Payment>) => {
     installment: data.installment || 1,
     totalInstallments: data.total_installments || 24,
     attachments: (data.attachments as unknown as string[]) || [],
-  };
+  } as Payment;
 };
 
 export const update = async (
